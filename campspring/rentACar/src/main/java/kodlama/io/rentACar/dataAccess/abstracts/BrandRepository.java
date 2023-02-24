7ae08fd04e17ba91aff9dd04ff29bd2eacdfd7aa
yakupbingöl <20010310007@ogrenci.bartin.edu.tr>
2023-02-24 17:33:49 +0300

    RentACar

---
+++ campspring/rentACar/src/main/java/kodlama/io/rentACar/dataAccess/abstracts/BrandRepository.java
@@ -0,0 +1,17 @@
+package kodlama.io.rentACar.dataAccess.abstracts;
+
+import java.util.List;
+
+
+
+import kodlama.io.rentACar.entities.concretes.Brand;
+
+
+public interface BrandRepository {
+	List<Brand> getAll ();
+
+	
+	
+	
+
+}
